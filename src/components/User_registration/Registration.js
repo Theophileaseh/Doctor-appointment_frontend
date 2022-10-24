2e8289e042035907debcef60e8721cd65dc981d6
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-10-25 00:28:17 +0100

    Add login page to project and sytle

---
+++ src/components/User_registration/Registration.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Registration.css';
+
+export const Login = () => {
+    return (
+        <>
+            <header>
+                <Nav />
+            </header>
+            <div className='main-section'>
+                <h1 className='login'>Login</h1>
+                <form>
+                    <div className='form-input'>
+                        <label>Username</label>
+                        <input type="text" />
+                    </div>
+                    <div className='form-input'>
+                        <label>email</label>
+                        <input type="text" />
+                    </div>
+                    <div className='form-action'>
+                        <button type='submit' className='submit-action'>Sign in</button>
+                    </div>
+                </form>
+            </div>
+        </>
+    )
+}
