6116d1338b0a991d2f2073c996b9c441c27d8748
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-10-25 00:21:57 +0100

    Import react and other react built in methods

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Login } from './components/User_registration/Registration';
 
 function App() {
   return (
