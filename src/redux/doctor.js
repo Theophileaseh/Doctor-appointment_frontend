612ce55350aad5f56507a305dab7573583b5a9af
Theophileaseh <theophilenwachan@gmail.com>
2022-10-29 13:51:04 +0100

    Create doctor reducer and actions

--- src/redux/doctor.js
+++ src/redux/doctor.js
@@ -1,43 +1,25 @@
 import axios from '../base/axios';
 
-const GET_COUNTRIES = 'covid19updates/redux/GET_COUNTRIES';
-const SELECT_COUNTRY = 'covid19updates/redux/SELECT_COUNTRY';
-const REMOVE_SELECTED_COUNTRY = 'covid19updates/redux/REMOVE_SELECTED_COUNTRY';
+const GET_DOCTORS = 'doctora/redux/GET_DOCTORS';
 
-const countryReducer = (state = [], action) => {
+const doctorReducer = (state = [], action) => {
   switch (action.type) {
-    case GET_COUNTRIES:
+    case GET_DOCTORS:
       return action.payload;
-    case SELECT_COUNTRY:
-      return action.payload;
-    case REMOVE_SELECTED_COUNTRY:
-      return {};
 
     default:
       return state;
   }
 };
 
-export const getCountries = () => (dispatch) => {
-  axios.get('summary').then((res) => {
+export const getDoctors = () => (dispatch) => {
+  axios.get('doctors').then((res) => {
     dispatch({
-      type: GET_COUNTRIES,
+      type: GET_DOCTORS,
       payload: res.data,
     });
   });
 };
 
-export const selectedcountry = () => (dispatch) => {
-  axios.get('summary').then((res) => {
-    dispatch({
-      type: SELECT_COUNTRY,
-      payload: res.data,
-    });
-  });
-};
 
-export const removeSelectedCountry = () => ({
-  type: REMOVE_SELECTED_COUNTRY,
-});
-
-export default countryReducer;
+export default doctorReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import appointmentModalReducer from './appointmentModal';
+import doctorReducer from './doctor';
 
 export default configureStore({
   reducer: {
     appointmentModal: appointmentModalReducer,
+    doctor: doctorReducer,
   },
   middleware: [logger, thunk],
 });
