01f6a26067ec04757d42e9dbc216786baef079ac
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-10-30 13:18:50 +0100

    change for label and fix linter errors

--- src/components/User_registration/SignUp.js
+++ src/components/User_registration/SignUp.js
@@ -1,28 +1,25 @@
-import React from 'react'
-import './Registration.css'
+import React from 'react';
+import './Registration.css';
 
-export const SignUp = () => {
-    return (
-        <>
-        <header>
-            <Nav />
-        </header>
-        <div className='main-section'>
-            <h1 className='login'>Sign Up</h1>
-            <form>
-                <div className='form-input'>
-                    <label>Username</label>
-                    <input type="text" />
-                </div>
-                <div className='form-input'>
-                    <label>email</label>
-                    <input type="text" />
-                </div>
-                <div className='form-action'>
-                    <button type='submit' className='submit-action'>Sign in</button>
-                </div>
-            </form>
+const SignUp = () => (
+  <>
+    <div className="main-section">
+      <h1 className="login">Sign Up</h1>
+      <form>
+        <div className="form-input">
+          <label htmlFor="userName">Email</label>
+          <input id="userName" type="text" />
         </div>
-        </>
-    )
-}
+        <div className="form-input">
+          <label>Password</label>
+          <input type="text" />
+        </div>
+        <div className="form-action">
+          <button type="submit" className="submit-action">Sign in</button>
+        </div>
+      </form>
+    </div>
+  </>
+);
+
+export default SignUp;
