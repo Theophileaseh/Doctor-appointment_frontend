14473a9e7458f795ad48ed572ee446cd3e2832ee
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-10-30 13:04:09 +0100

    Make changes to entry methods to fix linters error

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import './App.css';
-import { Login } from './components/User_registration/Registration';
-import { SignUp } from './components/User_registration/SignUp';
+import Login from './components/User_registration/Registration';
+import SignUp from './components/User_registration/SignUp';
 
 function App() {
   return (
     <Router>
-      <Routes>
       <Nav />
-      <Route path='/Registration' element={<Login />}/>
-      <Route path='/Sign_up' element={<SignUp />} />
+      <Routes>
+        <Route path="/Registration" exact element={<Login />} />
+        <Route path="/Sign_up" exact element={<SignUp />} />
       </Routes>
     </Router>
   );
