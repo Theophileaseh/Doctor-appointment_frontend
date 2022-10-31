03ba491bd0fffa4c2fc295986bb7caa56d0141a6
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-10-31 09:15:20 +0100

    Update Nav to current match branch

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-<<<<<<< HEAD
-import appointmentModalReducer from './appointmentModal';
-import doctorReducer from './doctor';
-import appointmentReducer from './appointment';
-
-export default configureStore({
-  reducer: {
-    appointmentModal: appointmentModalReducer,
-    doctor: doctorReducer,
-    appointment: appointmentReducer,
-=======
 import userReducer from './user';
 
 export default configureStore({
   reducer: {
     user: userReducer,
->>>>>>> eddcf0b07fc1e4547607a33e9d6a113788ab08b3
   },
   middleware: [logger, thunk],
 });
