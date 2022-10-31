768006d8c8d60c3f2090d2da77dcb1a199429dd0
Theophileaseh <theophilenwachan@gmail.com>
2022-10-31 17:34:12 +0100

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import AddAppointment from './components/AddAppointment/AddAppointment';
 import Appointment from './components/Appointment/Appointment';
-=======
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Nav from './components/Nav/Nav';
 import Signup from './components/SignUp/Signup';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
-
 import './App.css';
 
 function App() {
@@ -22,20 +17,14 @@ function App() {
           <Nav />
           <Routes>
             <Route path="/appointments" exact element={<Appointment />} />
+            <Route path="/" exact element={<Home />} />
+            <Route path="/signup" exact element={<Signup />} />
+            <Route path="/login" exact element={<Login />} />
           </Routes>
         </Router>
       </div>
       <AddAppointment />
     </>
-
-    <Router>
-      <Nav />
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/signup" exact element={<Signup />} />
-        <Route path="/login" exact element={<Login />} />
-      </Routes>
-    </Router>
 
   );
 }
