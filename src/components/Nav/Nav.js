7628e1b28c9f42cf7ae91144ea4485551b5153e3
Theophileaseh <theophilenwachan@gmail.com>
2022-11-01 21:54:05 +0100

    Hide navigation

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,7 +4,7 @@ import { BiLeftArrow, BiRightArrow } from 'react-icons/bi';
 import { FaFacebook, FaPinterestP } from 'react-icons/fa';
 import { BsTwitter, BsVimeo } from 'react-icons/bs';
 import { TiSocialGooglePlus } from 'react-icons/ti';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { showModal } from '../../redux/appointmentModal';
 import logo from '../../assets/doctora.png';
 import './Nav.css';
@@ -19,58 +19,62 @@ function Nav() {
   const [sidebar, setSidebar] = useState(false);
   const activeLink = (isActive) => (isActive ? { background: '#98bf0c', color: '#fff' } : undefined);
 
+  const location = useLocation();
+
   return (
-    <div className={sidebar ? 'navbar active' : 'navbar'}>
-      <div className="navbar-menu">
-        <div className="navbar-menu-logo">
-          <img className="logo" src={logo} alt="logo" />
+    location.pathname === '/' || location.pathname === '/login' || location.pathname === '/signup' ? '' : (
+      <div className={sidebar ? 'navbar active' : 'navbar'}>
+        <div className="navbar-menu">
+          <div className="navbar-menu-logo">
+            <img className="logo" src={logo} alt="logo" />
+          </div>
+          <div className="navbar-main-menu">
+            <NavLink to="/" style={({ isActive }) => activeLink(isActive)} className="nav-links">Doctors</NavLink>
+            <NavLink style={({ isActive }) => activeLink(isActive)} className="nav-links" onClick={setModal}>Add Appointment</NavLink>
+            <NavLink to="/appointments" style={({ isActive }) => activeLink(isActive)} className="nav-links">My Appointments</NavLink>
+            <NavLink to="/c" style={({ isActive }) => activeLink(isActive)} className="nav-links">Add Doctor</NavLink>
+            <NavLink to="/d" style={({ isActive }) => activeLink(isActive)} className="nav-links">Remove Doctor</NavLink>
+            <button type="button" className="sign-out">Log out</button>
+          </div>
+          <div className="navbar-footer">
+            <div className="navbar-social">
+              <button type="button" className="social-links">
+                <BsTwitter />
+              </button>
+              <button type="button" className="social-links">
+                <FaFacebook />
+              </button>
+              <button type="button" className="social-links">
+                <TiSocialGooglePlus />
+              </button>
+              <button type="button" className="social-links">
+                <BsVimeo />
+              </button>
+              <button type="button" className="social-links">
+                <FaPinterestP />
+              </button>
+            </div>
+            <div className="navbar-footer-info">
+              <p className="copyright">
+                &copy; &nbsp;
+                {new Date().getFullYear()}
+                {' '}
+                The Team
+              </p>
+            </div>
+
+          </div>
         </div>
-        <div className="navbar-main-menu">
-          <NavLink to="/" style={({ isActive }) => activeLink(isActive)} className="nav-links">Doctors</NavLink>
-          <NavLink style={({ isActive }) => activeLink(isActive)} className="nav-links" onClick={setModal}>Add Appointment</NavLink>
-          <NavLink to="/appointments" style={({ isActive }) => activeLink(isActive)} className="nav-links">My Appointments</NavLink>
-          <NavLink to="/c" style={({ isActive }) => activeLink(isActive)} className="nav-links">Add Doctor</NavLink>
-          <NavLink to="/d" style={({ isActive }) => activeLink(isActive)} className="nav-links">Remove Doctor</NavLink>
-          <button type="button" className="sign-out">Log out</button>
-        </div>
-        <div className="navbar-footer">
-          <div className="navbar-social">
-            <button type="button" className="social-links">
-              <BsTwitter />
-            </button>
-            <button type="button" className="social-links">
-              <FaFacebook />
-            </button>
-            <button type="button" className="social-links">
-              <TiSocialGooglePlus />
-            </button>
-            <button type="button" className="social-links">
-              <BsVimeo />
-            </button>
-            <button type="button" className="social-links">
-              <FaPinterestP />
+        <div className="navbar-toggle">
+          <div className="navbar-toggler">
+            <button type="button" className="navbar-button" onClick={() => setSidebar(!sidebar)}>
+              {sidebar ? <BiLeftArrow className="toggle-icon" /> : <BiRightArrow className="toggle-icon" /> }
             </button>
           </div>
-          <div className="navbar-footer-info">
-            <p className="copyright">
-              &copy; &nbsp;
-              {new Date().getFullYear()}
-              {' '}
-              The Team
-            </p>
-          </div>
-
         </div>
-      </div>
-      <div className="navbar-toggle">
-        <div className="navbar-toggler">
-          <button type="button" className="navbar-button" onClick={() => setSidebar(!sidebar)}>
-            {sidebar ? <BiLeftArrow className="toggle-icon" /> : <BiRightArrow className="toggle-icon" /> }
-          </button>
-        </div>
-      </div>
 
-    </div>
+      </div>
+    )
   );
 }
 
