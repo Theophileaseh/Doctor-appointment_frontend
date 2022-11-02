b6ef7585b839e769399cf9c28462f0b5e0df9701
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-11-02 01:50:36 +0100

    Rsolve merge conflict

--- src/components/Doctors/Doctors.js
+++ src/components/Doctors/Doctors.js
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from '../../base/axios';
 import './Doctors.css';
 
-const Doctors = () => {
-
+function Doctors() {
   const [data, setData] = useState([]);
   const allDoctors = () => { axios.get('doctors').then((res) => { setData(res.data); }); };
 
@@ -17,30 +15,17 @@ const Doctors = () => {
     <div className="doctors-container">
       <div className="doctors-section">
         {data.map((doctor) => (
-        <Link to={`/doctors/${doctor.id}`} >
-        <div className="single-doctor">
-          <img src={doctor.picture} alt="doctor" className="doctor-image" />
-          <p className="doctor-name">{doctor.name}</p>
-        </div>
-        </Link>
+          // eslint-disable-next-line react/jsx-key
+          <Link to={`/doctors/${doctor.id}`}>
+            <div className="single-doctor">
+              <img src={doctor.picture} alt="doctor" className="doctor-image" />
+              <p className="doctor-name">{doctor.name}</p>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default Doctors;
-=======
-import React from 'react';
-import './Doctors.css';
-
-const Doctors = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default Doctors;
->>>>>>> 939347290aa81232e7d0186fa8fd0f139494b506
