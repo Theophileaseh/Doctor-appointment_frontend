1427e2ec0fb22f7cd0a034fc8a603901477e1f0d
NWACHAN THEOPHILE <theophilenwachan@gmail.com>
2022-11-04 13:48:22 +0100

    Update Appointment.js

--- src/components/Appointment/Appointment.js
+++ src/components/Appointment/Appointment.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import axios from '../../base/axios';
 import './Appointment.css';
 
 function Appointment() {
